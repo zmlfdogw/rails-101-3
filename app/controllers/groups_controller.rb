f6aa679aae89c18eccaa6ad30027ce41b2c4b063
zmlfdogw <chinamichaelzhang@192.168.2.2>
2016-07-22 03:10:11 +0800

    install devise

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user! , only: [:new]
+  
   def index
     @groups = Group.all
   end
@@ -34,7 +36,7 @@ class GroupsController < ApplicationController
       render :edit
     end
   end
-  
+
   def destroy
     @group = Group.find(params[:id])
     @group.destroy
