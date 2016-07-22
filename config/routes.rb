cf63ee746d331b4f5d8c3f13ab146a96f685dfe7
zmlfdogw <chinamichaelzhang@192.168.2.2>
2016-07-22 23:56:27 +0800

    implement my post

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :groups do
-      member do
-        post :join
-        post :quit
-      end
+    member do
+      post :join
+      post :quit
+    end
+
     resources :posts
   end
 
   namespace :account do
     resources :groups
+    resources :posts
   end
   root 'groups#index'
 end
